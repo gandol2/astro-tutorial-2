9c93f4d9b834a57321eee897613e73c999c0a75b
gandol2 <sskim2333@gmail.com>
2026-06-24 04:00:43 +0900

    tmp

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,18 @@
 // @ts-check
 import { defineConfig } from "astro/config";
+import sitemap from "@astrojs/sitemap";
+
+import metaTags from "astro-meta-tags";
 
 // https://astro.build/config
 export default defineConfig({
 	// 여기에 구성 옵션을 작성합니다.
 
-	site: "https://astross.mycafe24.com", // 프로덕션 사이트 URL 지정, 코드펜스에서 import.meta.env.SITE 로 사용할수 있음
-	compressHTML: false, // 빌드 결과물 HTML을 보기 좋게 포메팅
+	// 프로덕션 사이트 URL 지정, 코드펜스에서 import.meta.env.SITE 로 사용할수 있음
+	site: "https://astross.mycafe24.com",
+
+	// 빌드 결과물 HTML을 보기 좋게 포메팅
+	compressHTML: false,
 
 	build: {
 		format: "file", // 빌드 결과물을 ./dist/page.html 형식으로 출력
@@ -14,9 +20,12 @@ export default defineConfig({
 		assets: "assets",
 	},
 
-	scopedStyleStrategy: "class", // css selector 규칙을 class name 기반으로 출력
+	// css selector 규칙을 class name 기반으로 출력
+	scopedStyleStrategy: "class",
 
 	image: {
 		responsiveStyles: true,
 	},
+
+	integrations: [metaTags(), sitemap()],
 });
---
+++ src/pages/blogs/post1.md
@@ -0,0 +1,7 @@
+---
+import "layout.astro"
+
+title: 새로운 이빨을 소개합니다.
+auth: 라ㅓ임러
+---
+# 
